af5231a7f99b7d8d863d36dccfaff1c6ed785ba5
CTZDev <carlosteranzavaleta@hotmail.com>
2021-08-16 21:28:25 -0500

    add jobs filter

--- app/js/modules/cards.js
+++ app/js/modules/cards.js
@@ -1,4 +1,7 @@
 const d = document;
+const $fragment = d.createDocumentFragment();
+let jobsFilter = [];
+const jobs = [];
 
 const getJobs = async () => {
   try {
@@ -13,13 +16,14 @@ const getJobs = async () => {
   }
 };
 
+const deleteDuplicates = (array) => (array = [...new Set([...array])]);
+
 const drawJobs = (data) => {
-  console.log(data);
   const $template = d.getElementById("template-card").content;
-  const $fragment = d.createDocumentFragment();
   const $cards = d.querySelector(".cards");
 
   data.forEach((el) => {
+    const jobsData = [];
     let $clone = $template.cloneNode(true);
     $clone.querySelector(".card-main > img").setAttribute("src", el.logo);
     $clone.querySelector(".card-main > img").setAttribute("alt", el.company);
@@ -33,16 +37,95 @@ const drawJobs = (data) => {
     $clone.querySelector("[data-footer-role]").textContent = el.role;
     $clone.querySelector("[data-footer-level]").textContent = el.level;
     el.languages.forEach((lang) => {
-      $clone.querySelector(".card-footer-items").insertAdjacentHTML("beforeend", `<li class="tooltip">${lang}</li>`);
+      $clone.querySelector(".card-footer-items").insertAdjacentHTML("beforeend", `<button type="button" class="tooltip">${lang}</button>`);
+      jobsData.push(lang);
     });
     el.tools.forEach((tool) => {
-      $clone.querySelector(".card-footer-items").insertAdjacentHTML("beforeend", `<li class="tooltip">${tool}</li>`);
+      $clone.querySelector(".card-footer-items").insertAdjacentHTML("beforeend", `<button type="button" class="tooltip">${tool}</button>`);
+      jobsData.push(tool);
     });
-
+    jobsData.push(el.role, el.level);
+    jobs.push(jobsData);
     $fragment.append($clone);
   });
 
   $cards.append($fragment);
 };
+
+const generateFeature = (text) => {
+  const $template = d.getElementById("template-filter-tooltip").content;
+  const $filterContainer = d.querySelector(".tooltip-control");
+
+  jobsFilter.push(text);
+  const jobsClean = deleteDuplicates(jobsFilter);
+  $filterContainer.innerHTML = "";
+
+  jobsClean.forEach((job) => {
+    let $clone = $template.cloneNode(true);
+    $clone.querySelector(".tooltip > p").textContent = job;
+    $fragment.appendChild($clone);
+  });
+
+  $filterContainer.append($fragment);
+};
+
+d.addEventListener("click", (e) => {
+  const $filter = d.querySelector(".filter");
+
+  //CAPTURE FILTER JOBS
+  if (e.target.matches(".card-footer-items > *")) {
+    const $textFeatures = e.target.textContent;
+    $filter.classList.add("is-active");
+    generateFeature($textFeatures);
+
+    jobs.forEach((job, i) => {
+      let acumJobs = 0;
+      for (const jobFilter of jobsFilter) {
+        if (job.includes(jobFilter)) {
+          acumJobs++;
+          if (acumJobs === jobsFilter.length) {
+            d.querySelectorAll(".card")[i].classList.add("active-featured");
+          } else {
+            d.querySelectorAll(".card")[i].classList.remove("active-featured");
+          }
+        } else {
+          d.querySelectorAll(".card")[i].classList.add("is-active");
+        }
+      }
+    });
+  }
+
+  //DELETE FILTER JOBS
+  if (e.target.matches("#btnclose-tooltip")) {
+    const $tooltip = e.target.parentElement;
+    const $tooltipText = e.target.previousElementSibling.textContent;
+    jobsFilter = jobsFilter.filter((job) => job !== $tooltipText);
+    $tooltip.remove();
+
+    jobs.forEach((job, i) => {
+      let acumJobs = 0;
+      for (const jobFilter of jobsFilter) {
+        if (job.includes(jobFilter)) {
+          acumJobs++;
+          if (acumJobs === jobsFilter.length) {
+            d.querySelectorAll(".card")[i].classList.add("active-featured");
+            d.querySelectorAll(".card")[i].classList.remove("is-active");
+          } else {
+            d.querySelectorAll(".card")[i].classList.add("is-active");
+            d.querySelectorAll(".card")[i].classList.remove("active-featured");
+          }
+        }
+      }
+    });
+
+    if (jobsFilter.length === 0) {
+      d.querySelectorAll(".card").forEach((card) => {
+        card.classList.remove("is-active");
+        card.classList.remove("active-featured");
+      });
+      $filter.classList.remove("is-active");
+    }
+  }
+});
 
 export default getJobs;
